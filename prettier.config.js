f81b22f00d1a11868f27214b83e758cbedc658fa
Stefan Stölzle <stoe@github.com>
2022-08-16 12:34:50 +0200

    Delete not needed disabled eslint rule

--- prettier.config.js
+++ prettier.config.js
@@ -1,2 +1,2 @@
-// eslint-disable-next-line filenames/match-regex, import/extensions, import/no-commonjs
+// eslint-disable-next-line import/extensions, import/no-commonjs
 module.exports = require('eslint-plugin-github/prettier.config')
