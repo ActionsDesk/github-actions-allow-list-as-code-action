4acd71433cc69302c27da09b6aef2775c9bd61fa
Stefan Stölzle <stoe@github.com>
2023-04-03 09:13:22 +0200

    💥 Refactor action (#80)
    Do not use `.mjs`

---
+++ .prettierrc.cjs
@@ -0,0 +1,2 @@
+// eslint-disable-next-line filenames/match-regex, import/extensions, import/no-commonjs
+module.exports = require('eslint-plugin-github/prettier.config')
--- prettier.config.js
+++
@@ -1,2 +0,0 @@
-// eslint-disable-next-line import/extensions, import/no-commonjs
-module.exports = require('eslint-plugin-github/prettier.config')
