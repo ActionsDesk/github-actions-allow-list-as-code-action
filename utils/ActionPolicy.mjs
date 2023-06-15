10334d344ba63aa8036925449e26458fb3456eaf
Damien Hardy <49808393+digimangos@users.noreply.github.com>
2023-06-15 16:31:53 +0100

    Add proxy agent

--- utils/ActionPolicy.mjs
+++ utils/ActionPolicy.mjs
@@ -2,7 +2,7 @@ import {readFileSync} from 'fs'
 import {Octokit} from '@octokit/core'
 import {enterpriseCloud} from '@octokit/plugin-enterprise-cloud'
 import {load} from 'js-yaml'
-import ProxyAgent from 'proxy-agent';
+import ProxyAgent from 'proxy-agent'
 
 const MyOctokit = Octokit.plugin(enterpriseCloud)
 
@@ -47,7 +47,7 @@ class ActionPolicy {
       baseUrl: ghApiUrl,
       request: {
         agent: new ProxyAgent(),
-      }
+      },
     })
 
     if (!enterprise && !organization) {
